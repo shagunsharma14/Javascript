aee3844dfd009692afe64f2debed70423f1bc288
shagunsharma14 <shaguns880@gmail.com>
2024-06-11 21:18:46 +0530

    Practice code

---
+++ first.js
@@ -0,0 +1,19 @@
+name = "Tony starK";
+age = 24;
+price = 99.99;
+x = null;
+console.log(age);
+console.log(price);
+console.log(name); 
+const student = {fullName: "Rahul kumar", age: 20, cgpa: 8.2, isPass: true};
+console.log(student)
+student["age"] = student["age"]+1
+console.log(student)
+const profile = {
+    userName: "shradhakhapra",
+    post: 195,
+    followers: 569000,
+    following: 4,
+    fullName: "Shradha Khapra",
+    about: "Apna College.."
+};
