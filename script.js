9347b3f6ceb7938de0ab55b15d376598ad507444
shagunsharma14 <shaguns880@gmail.com>
2024-06-16 21:48:49 +0530

    dom

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-
-
-let marks = prompt("Enter the marks: ");
-if(marks>=80 && marks<=100){
-    console.log("A");
-}else if(marks>=70 && marks<=89){
-    console.log("B");
-}else if(marks>=60 && marks<=69){
-    console.log("C");
-}else if(marks>=50 && marks<=59){
-    console.log("D");
-}else if(marks>=0 && marks<=49){
-    console.log("F");
-}else{
-    console.log("Invalid Input");
+// let h2 = document.querySelector("body > h2");
+// console.log(h2.innerText);
+// let text = h2.innerText+" Apna College"
+// h2.innerText = text;
+let divs = document.getElementsByClassName("box");
+let idx = 1;
+for (let div of divs){
+    div.innerText =  `New element index is: ${idx}`;
+    idx++;
+    console.log(div.innerText);
 }
