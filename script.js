8f02054c4944c4a1be4e236d3decec0e139cf055
shagunsharma14 <shaguns880@gmail.com>
2024-06-17 21:28:48 +0530

    DOM p2

--- script.js
+++ script.js
@@ -2,10 +2,25 @@
 // console.log(h2.innerText);
 // let text = h2.innerText+" Apna College"
 // h2.innerText = text;
-let divs = document.getElementsByClassName("box");
-let idx = 1;
-for (let div of divs){
-    div.innerText =  `New element index is: ${idx}`;
-    idx++;
-    console.log(div.innerText);
-}
+// let divs = document.getElementsByClassName("box");
+// let idx = 1;
+// for (let div of divs){
+//     div.innerText =  `New element index is: ${idx}`;
+//     idx++;
+//     console.log(div.innerText);
+// } 
+
+// //Practice question
+// let button = document.createElement("button");
+// button.innerText = "click me";  
+
+// button.style.backgroundColor = "red";
+// button.style.color = "white";
+
+// console.dir(button);
+// document.querySelector("body").prepend(button);
+
+
+//Practice Question
+// let para = document.querySelector("p");
+// para.classList.add("newClass");
