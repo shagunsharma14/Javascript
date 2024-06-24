8c8a68f88d5947fd26665742021f1462a8f9e047
shagunsharma14 <shaguns880@gmail.com>
2024-06-24 20:50:02 +0530

    fetch-api

---
+++ api.js
@@ -0,0 +1,24 @@
+const URL = "https://cat-fact.herokuapp.com/facts";
+const factPara = document.querySelector("#fact");
+const btn = document.querySelector("#btn");
+
+
+// const getFacts = async ()=>{
+//     console.log("getting facts...");
+//     let response = await fetch(URL);
+//     let data = await response.json();
+//     factPara.innerText = data[2].text;
+// };
+
+
+function getFacts(){
+    fetch(URL)
+    .then((response)=>{
+        return response.json();
+    })
+    .then((data)=>{
+        console.log(data);
+        factPara.innerText = data[2].text;
+    })
+}
+btn.addEventListener("click", getFacts);
